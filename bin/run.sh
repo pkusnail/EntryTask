4188725a386859e0ab771d0c495d121331ead41f
root <alejandroseaah@pku.edu.cn>
2018-08-18 03:15:53 +0000

    add start/stop cmd and other functions

--- bin/run.sh
+++ bin/run.sh
@@ -1,25 +1,95 @@
 
 #!/bin/sh
 
-path=`dirname $0`
-curr=`pwd`
-exe=$curr/$path/../pkg
+### functions definition :
 
-#ps -ef | grep tcp_server | grep -v grep | grep -v tail | awk {'print $2'}  | xargs kill -9
-#ps -ef | grep web_server | grep -v grep | grep -v tail | awk {'print $2'}  | xargs kill -9
+killFunc()
+{
+    strs=`netstat -nap | grep $@ | grep LISTEN | awk {' print $7'}`
 
-strs=`netstat -nap | grep tcp_server | grep LISTEN | awk {' print $7'}`
-IFS='/' read -r -a array <<< "$strs"
-echo "killing tcp_server pid ${array[0]}"
-kill -9 "${array[0]}"
+    if [ ${#strs} -eq 0 ]; then
+        return
+    fi
+    echo "killing  $@"
+    IFS='/' read -r -a array <<< "$strs"
+    echo "killing  pid ${array[0]}"
+    kill -9 "${array[0]}"
+}
 
-strs=`netstat -nap | grep web_server | grep LISTEN | awk {' print $7'}`
-IFS='/' read -r -a array <<< "$strs"
-echo "killing web_server pid ${array[0]}"
-kill -9 "${array[0]}"
+startFunc()
+{
+    cd $exe/$1
+    nohup ./$2 &
+}
 
-cd $exe/tcp
-nohup ./tcp_server &
-cd $exe/web
-nohup ./web_server &
+statusFunc()
+{
+    strs=`netstat -nap | grep tcp_server | grep LISTEN | awk {' print $7'}`
+    if [ ${#strs} -eq 0 ]; then
+        echo "tcp server not working"
+    else
+        echo $strs
+    fi
+
+    strs=`netstat -nap | grep web_server | grep LISTEN | awk {' print $7'}`
+    if [ ${#strs} -eq 0 ]; then
+        echo "web server not working"
+    else
+        echo $strs
+    fi
+}
+
+usage()
+{
+    echo "usage:"
+    echo "To check status:"
+    echo "  sh run.sh status"
+    echo "To start web server or / and tcp server:"
+    echo "  sh run.sh start web | tcp | all"
+    echo "To start web server or / and tcp server:"
+    echo "  sh run.sh stop web | tcp | all"
+}
+
+### main function :
+
+if [ $# -eq 0 ]; then
+    usage
+elif [ $# -eq 1 ]; then
+    if [ $1 = "status" ];then
+        statusFunc
+    else
+        echo " parameter error"
+        usage
+    fi
+elif [ $# -eq 2 ]; then
+    if [ $1 = "start" ];then
+
+        path=`dirname $0`
+        curr=`pwd`
+        exe=$curr/$path/../pkg
+
+        if [ $2 = "tcp" ];then
+            startFunc tcp tcp_server
+        elif [ $2 = "web" ];then
+            startFunc web web_server
+        elif [ $2 = "all" ];then
+            startFunc tcp tcp_server
+            startFunc web web_server
+        fi
+    fi
+
+    if [ $1 = "stop" ];then
+        if [ $2 = "tcp" ];then
+            killFunc tcp_server
+        elif [ $2 = "web" ];then
+            killFunc web_server
+        elif [ $2 = "all" ];then
+            killFunc tcp_server
+            killFunc web_server
+        fi 
+    fi
+elif [ $# -gt 2 ];then
+    echo "parameter error"
+    usage
+fi
 
